b6d8742fbb44fa3beefe9bfe0c3835320f62ce10
No767 <73260931+No767@users.noreply.github.com>
2022-06-18 00:13:21 -0700

    Add project files

---
+++ README.md
@@ -0,0 +1,30 @@
+<div align="center">
+
+# Rin-Exceptions
+
+![Rin](https://raw.githubusercontent.com/No767/Rin/dev/assets/Rin%20Logo%20V4%20(GitHub).png)
+
+
+![PyPI](https://img.shields.io/pypi/v/rin-exceptions?label=PyPi&logo=pypi&logoColor=white) ![PyPI - Python Version](https://img.shields.io/pypi/pyversions/rin-exceptions?label=Python&logo=python&logoColor=white) ![PyPI - Downloads](https://img.shields.io/pypi/dd/rin-exceptions?label=PyPi%20Downloads&logo=pypi&logoColor=white) ![GitHub](https://img.shields.io/github/license/No767/Rin-Exceptions?label=License&logo=github)
+
+A set of base exceptions for Rin
+
+<div align="left">
+
+# Info
+
+This package is a set of base exceptions used by Rin. It's meant to contain custom exceptions that are used by Rin, and in turn, Kumiko.
+
+# Installation
+
+pip: 
+
+```sh
+pip install rin-exceptions
+```
+
+poetry:
+
+```sh
+poetry add rin-exceptions
+```
---
+++ src/rin_exceptions/main.py
@@ -0,0 +1,61 @@
+class Error(Exception):
+    """The base class for all exceptions. This should not be explicitly raised or called.
+
+    Args:
+        Exception (Exception): Base exception for this class
+    """
+    pass
+
+
+class UnknownPlayer(Error):
+    """Raised when there is no player found within the Hypixel Cog.
+
+    Args:
+        Error (_type_): Base Error
+    """
+    pass
+
+
+class NotFoundHTTPException(Error):
+    """Raised when there is usually an HTTP 404 error within the API
+
+    Args:
+        Error (_type_): Base Error
+    """
+    pass
+
+
+class NoItemsError(Error):
+    """Raised when there are no items found. This should be raised instead of NotFoundHTTPException when the HTTP status is still 2xx-3xx.
+
+    Args:
+        Error (_type_): Base Error
+    """
+    pass
+
+
+class ThereIsaRSlashInSubreddit(Error):
+    """Raised when there is an r/ within the subreddit name. This should be only raised in the Reddit Cog. 
+
+    Args:
+        Error (_type_): Base Error
+    """
+    pass
+
+
+class HTTPException(Error):
+    """Raised when the HTTP status of the request is 5xx
+
+    Args:
+        Error (_type_): Base Error
+    """
+    pass
+
+
+class InvalidToken(Error):
+    """Raised when the token being used for authentication is invalid.
+
+    Args:
+        Error (_type_): Base Error
+    """
+    pass
